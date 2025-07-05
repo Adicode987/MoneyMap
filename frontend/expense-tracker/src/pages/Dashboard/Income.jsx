f25dfa21e4b16bd6292a2ad81dd099121214ab19
AdityakKumar <2023csb1096@iitrpr.ac.in>
2025-07-05 17:52:01 +0530

    Income Page Completed

--- frontend/expense-tracker/src/pages/Dashboard/Income.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Income.jsx
@@ -8,6 +8,8 @@ import Modal from "../../components/Modal";
 import AddIncomeForm from "../../components/Income/AddIncomeForm";
 import { useUserAuth } from "../../hooks/useUserAuth";
 import toast from "react-hot-toast";
+import IncomeList from "../../components/Income/IncomeList";
+import DeleteAlert from "../../components/DeleteAlert";
 
 const Income = () => {
   useUserAuth();
@@ -81,10 +83,51 @@ const Income = () => {
     }
   };
 
+  //Delete Income
+  const deleteIncome = async (id) => {
+    try {
+      await axiosInstance.delete(API_PATHS.INCOME.DELETE_INCOME(id));
+
+      setOpenDeleteAlert({ show: false, data: null });
+      toast.success("Income details deleted successfully");
+      fetchIncomeDetails();
+    } catch (error) {
+      console.error(
+        "Error deleting income:",
+        error.response?.data?.message || error.message
+      );
+    }
+  };
+
+  //handle download income details
+  const handleDownloadIncomeDetails = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.INCOME.DOWNLOAD_INCOME,
+        {
+          responseType: "blob",
+        }
+      );
+
+      // Create a URL for the blob
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", "income_details.xlsx");
+      document.body.appendChild(link);
+      link.click();
+      link.parentNode.removeChild(link);
+      window.URL.revokeObjectURL(url);
+    } catch (error) {
+      console.error("Error downloading income details:", error);
+      toast.error("Failed to download income details. Please try again.");
+    }
+  };
+
   useEffect(() => {
     fetchIncomeDetails();
 
-    return () => {};
+    return () => { };
   }, []);
 
   return (
@@ -97,15 +140,35 @@ const Income = () => {
               onAddIncome={() => setOpenAddIncomeModal(true)}
             />
           </div>
-        </div>
 
-        <Modal
+          <IncomeList
+            transactions={incomeData}
+            onDelete={(id) => {
+              setOpenDeleteAlert({ show: true, data: id });
+            }}
+            onDownload={handleDownloadIncomeDetails}
+          />
+          <Modal
             isOpen={openAddIncomeModal}
             onClose={() => setOpenAddIncomeModal(false)}
             title="Add Income"
           >
             <AddIncomeForm onAddIncome={handleAddIncome} />
           </Modal>
+
+          <Modal
+            isOpen={openDeleteAlert.show}
+            onClose={() => setOpenDeleteAlert({ show: false, data: null })}
+            title="Delete Income"
+          >
+            <DeleteAlert
+              content="Are you sure you want to delete this income detail?"
+              onDelete={() => deleteIncome(openDeleteAlert.data)}
+            />
+          </Modal>
+        </div>
+
+
 
       </div>
     </DashboardLayout>
