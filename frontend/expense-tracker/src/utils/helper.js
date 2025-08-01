e5eb48255af52a77e0232c9a88b6884801c83485
Rishabh-Rawat22 <2023mcb1377@iitrpr.ac.in>
2025-08-02 00:44:26 +0530

    Expense Page

--- frontend/expense-tracker/src/utils/helper.js
+++ frontend/expense-tracker/src/utils/helper.js
@@ -58,6 +58,8 @@ export const addThousandsSeparator = (num) => {
     : formattedInteger;
 };
 
+
+
 export const prepareExpenseBarChartData = (data = []) => {
   const chartData = data.map((item) => ({
     category: item?.category,
@@ -82,6 +84,18 @@ export const prepareIncomeBarChartData = (data = []) => {
     month: moment(item?.date).format('Do MMM'),
     amount: item?.amount,
     source: item?.source,
+  }));
+
+  return chartData;
+};
+
+export const prepareExpenseLineChartData = (data = []) => {
+  const sortedData = [...data].sort((a, b) => new Date(a.date) - new Date(b.date));
+
+  const chartData = sortedData.map((item) => ({
+    month: moment(item?.date).format('Do MMM'),
+    amount: item?.amount,
+    category: item?.category,
   }));
 
   return chartData;
